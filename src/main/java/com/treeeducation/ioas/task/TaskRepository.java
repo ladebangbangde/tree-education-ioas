fbb9fe1f5e889de72e1a341b3eea04304897c216
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-20 15:14:41 +0800

    feat: add task cleanup and concurrency repository queries

--- src/main/java/com/treeeducation/ioas/task/TaskRepository.java
+++ src/main/java/com/treeeducation/ioas/task/TaskRepository.java
@@ -2,6 +2,8 @@ package com.treeeducation.ioas.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,4 +11,8 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByRoleType(TaskRoleType roleType);
     Optional<Task> findFirstByTaskTypeAndRelatedPackageId(TaskType taskType, Long relatedPackageId);
     long countByRoleTypeAndStatus(TaskRoleType roleType, String status);
+    long countByRoleTypeAndStatusIn(TaskRoleType roleType, Collection<String> statuses);
+    long countByRoleTypeAndAssigneeIdAndStatusIn(TaskRoleType roleType, Long assigneeId, Collection<String> statuses);
+    List<Task> findByStatusInAndUpdatedAtBefore(Collection<String> statuses, Instant updatedAtBefore);
+    List<Task> findByStatusInAndCompletedAtBefore(Collection<String> statuses, Instant completedAtBefore);
 }
