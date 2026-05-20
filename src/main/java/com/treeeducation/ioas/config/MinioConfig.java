f25d1df761eddbeae5b5911d904092e441a09990
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-20 16:33:21 +0800

    feat: add public minio client for presigned uploads

--- src/main/java/com/treeeducation/ioas/config/MinioConfig.java
+++ src/main/java/com/treeeducation/ioas/config/MinioConfig.java
@@ -1,6 +1,7 @@
 package com.treeeducation.ioas.config;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,9 @@ public class MinioConfig {
     @Value("${ioas.storage.endpoint}")
     private String endpoint;
 
+    @Value("${ioas.storage.public-endpoint:${ioas.storage.endpoint}}")
+    private String publicEndpoint;
+
     @Value("${ioas.storage.access-key}")
     private String accessKey;
 
@@ -18,9 +22,19 @@ public class MinioConfig {
     private String secretKey;
 
     @Bean
+    @Qualifier("minioClient")
     public MinioClient minioClient() {
         return MinioClient.builder()
                 .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("publicMinioClient")
+    public MinioClient publicMinioClient() {
+        return MinioClient.builder()
+                .endpoint(publicEndpoint)
                 .credentials(accessKey, secretKey)
                 .build();
     }
