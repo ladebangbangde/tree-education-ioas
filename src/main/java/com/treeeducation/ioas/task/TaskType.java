3490add61237f60f832af673cf0f1002708dbb71
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-21 13:54:44 +0800

    feat: add package create task type

--- src/main/java/com/treeeducation/ioas/task/TaskType.java
+++ src/main/java/com/treeeducation/ioas/task/TaskType.java
@@ -4,5 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "任务类型")
 public enum TaskType {
-    media_upload, operator_lead_generate
+    package_create,
+    media_upload,
+    operator_lead_generate
 }
