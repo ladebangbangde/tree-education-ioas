4203622705b3e7ed59641c1ed679ff8dd3f0dc5f
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-21 14:11:03 +0800

    feat: configure s3 client and presigner for minio multipart

--- src/main/java/com/treeeducation/ioas/config/MinioConfig.java
+++ src/main/java/com/treeeducation/ioas/config/MinioConfig.java
@@ -5,6 +5,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.net.URI;
 
 @Configuration
 public class MinioConfig {
@@ -21,6 +29,9 @@ public class MinioConfig {
     @Value("${ioas.storage.secret-key}")
     private String secretKey;
 
+    @Value("${ioas.storage.region:us-east-1}")
+    private String region;
+
     @Bean
     @Qualifier("minioClient")
     public MinioClient minioClient() {
@@ -36,6 +47,28 @@ public class MinioConfig {
         return MinioClient.builder()
                 .endpoint(publicEndpoint)
                 .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("internalS3Client")
+    public S3Client internalS3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of(region))
+                .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
+                .build();
+    }
+
+    @Bean
+    @Qualifier("publicS3Presigner")
+    public S3Presigner publicS3Presigner() {
+        return S3Presigner.builder()
+                .endpointOverride(URI.create(publicEndpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of(region))
+                .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
                 .build();
     }
 }
