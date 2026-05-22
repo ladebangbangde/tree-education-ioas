6d8586f1e846ceb1eb8d5fb1a42cbd0e37d8da0f
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-22 18:54:47 +0800

    feat: add official lead repository

---
+++ src/main/java/com/treeeducation/ioas/official/lead/OfficialLeadRepository.java
@@ -0,0 +1,6 @@
+package com.treeeducation.ioas.official.lead;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfficialLeadRepository extends JpaRepository<OfficialLead, Long> {
+}
