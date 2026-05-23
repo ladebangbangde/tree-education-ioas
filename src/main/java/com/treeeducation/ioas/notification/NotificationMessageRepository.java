a184448f43e8d1a393b5a22bd299949358c58893
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-24 02:22:04 +0800

    feat: extend notification repository queries

--- src/main/java/com/treeeducation/ioas/notification/NotificationMessageRepository.java
+++ src/main/java/com/treeeducation/ioas/notification/NotificationMessageRepository.java
@@ -1,7 +1,9 @@
 package com.treeeducation.ioas.notification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface NotificationMessageRepository extends JpaRepository<NotificationMessage, Long> {
     long countByReceiverUserIdAndReadStatus(Long receiverUserId, String readStatus);
+    List<NotificationMessage> findTop20ByReceiverUserIdOrderByCreatedAtDesc(Long receiverUserId);
 }
