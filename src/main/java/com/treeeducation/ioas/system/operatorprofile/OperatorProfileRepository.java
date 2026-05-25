37aeae6085c8fd02964004c61cd8af62ac96174e
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-26 00:18:03 +0800

    Add operator profile lookup by user

--- src/main/java/com/treeeducation/ioas/system/operatorprofile/OperatorProfileRepository.java
+++ src/main/java/com/treeeducation/ioas/system/operatorprofile/OperatorProfileRepository.java
@@ -3,7 +3,9 @@ package com.treeeducation.ioas.system.operatorprofile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OperatorProfileRepository extends JpaRepository<OperatorProfile, Long> {
     List<OperatorProfile> findByEnabledTrueOrderByNameAsc();
+    Optional<OperatorProfile> findByUserId(Long userId);
 }
