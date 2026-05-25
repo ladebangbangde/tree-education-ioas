a6977b376238bd6272af19b91ea6cc563cc59316
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-25 18:44:55 +0800

    Add lead converted and closed statuses

--- src/main/java/com/treeeducation/ioas/lead/LeadStatus.java
+++ src/main/java/com/treeeducation/ioas/lead/LeadStatus.java
@@ -4,5 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "线索状态")
 public enum LeadStatus {
-    unassigned, assigned, following, completed, invalid
+    unassigned,
+    assigned,
+    following,
+    confirmed,
+    converted,
+    completed,
+    invalid,
+    closed
 }
