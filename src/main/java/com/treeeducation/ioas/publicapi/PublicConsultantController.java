e393517229e58449aabbbc7a4daf0a1cd423898c
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-26 12:35:33 +0800

    Add public consultant listing endpoint

---
+++ src/main/java/com/treeeducation/ioas/publicapi/PublicConsultantController.java
@@ -0,0 +1,29 @@
+package com.treeeducation.ioas.publicapi;
+
+import com.treeeducation.ioas.common.ApiResponse;
+import com.treeeducation.ioas.profile.ProfileDtos;
+import com.treeeducation.ioas.profile.ProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/public/consultants")
+@Tag(name = "Public Consultants", description = "官网公开顾问展示接口")
+public class PublicConsultantController {
+    private final ProfileService profileService;
+
+    public PublicConsultantController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @GetMapping
+    @Operation(summary = "官网公开顾问展示列表")
+    public ApiResponse<List<ProfileDtos.PublicConsultantCardResponse>> list() {
+        return ApiResponse.ok(profileService.publicConsultants());
+    }
+}
