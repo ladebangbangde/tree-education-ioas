9db04fe0b9f53d3deef4ec1944022a0fecad4a75
ladebangbangde <89012621+ladebangbangde@users.noreply.github.com>
2026-05-28 17:38:36 +0800

    Add advisor profile repository

---
+++ src/main/java/com/treeeducation/ioas/system/advisor/AdvisorProfileRepository.java
@@ -0,0 +1,8 @@
+package com.treeeducation.ioas.system.advisor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface AdvisorProfileRepository extends JpaRepository<AdvisorProfile, Long> {
+    Optional<AdvisorProfile> findByUserId(Long userId);
+}
